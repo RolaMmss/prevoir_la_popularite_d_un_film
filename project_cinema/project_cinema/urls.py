f342511d33f4fb0b776b846f3c3c7cc0de932724
RolaMmss <rola.el-moallem@hotmail.fr>
2023-07-20 22:13:42 +0200

    views

--- project_cinema/project_cinema/urls.py
+++ project_cinema/project_cinema/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/',views.hello),
+    path('',views.homepage,name='homepage'),
+    path('signup/',views.signup, name='signup'),
+    path('login/',views.login_user, name='login'),
+    path('logout/',views.logout_user, name='logout'),
 ]
