b53729471b73e47ec6d6fc25215ef21815693920
MyriamLbhn <lebihan.myriam@gmail.com>
2023-07-21 01:15:31 +0200

    scraping boxoffice done

--- scrap_allocine/scrap_allocine/spiders/allocine_spider.py
+++ scrap_allocine/scrap_allocine/spiders/allocine_spider.py
@@ -2,43 +2,59 @@ import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from ..items import AllocineMovieItem
-import scrapy
 
-import scrapy
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
-from ..items import AllocineMovieItem
-
-class AllocineSpiderSpider(CrawlSpider):
+class AllocineSpider(CrawlSpider):
     name = "allocine_spider"
     allowed_domains = ["allocine.fr"]
-    start_urls = ["https://www.allocine.fr/boxoffice/france/sem-2023-04-19/"]
-
-    rules = (
-        Rule(LinkExtractor(restrict_css=".meta-title-link"), callback="parse_movie_details"),
-    )
 
     # Spécifiez les colonnes pour le CSV
-    fields = ['title', 'date', 'boxoffice']
+    fields = ['title', 'boxoffice']
+
     
-    def parse_start_url(self, response):
-        boxoffice = response.css('strong::text').get()
+    DEPTH_LIMIT = 5
+
+    # Spécifiez les colonnes pour le CSV
+    fields = ['title', 'boxoffice']
+    # URL de la première page
+    base_url = 'https://www.allocine.fr/films/decennie-2020/annee-2022'
+    num_pages = 200
+
+    def start_requests(self):
+        # Générer les URL de pagination
+        for page in range(1, self.num_pages + 1):
+            url = f"{self.base_url}/?page={page}"
+            yield scrapy.Request(url, callback=self.parse)
+
+    def parse(self, response):
+        # Récupérer les titres de films sur la page actuelle
+        titles = response.css("a.meta-title-link::text").getall()
+
+        # Récupérer les liens des films sur la page actuelle
+        film_links = response.css("a.meta-title-link::attr(href)").getall()
+
+        # Combinaison des titres et des liens des films
+        for title, film_link in zip(titles, film_links):
+            # Suivre le lien du film pour accéder à la page du film
+            yield response.follow(film_link, callback=self.parse_film, meta={'title': title.strip()})
+
+    def parse_film(self, response):
+        # Extraire le titre du film à partir des informations stockées dans la méta
+        title = response.meta['title']
+        # Extraire le numéro du film à partir de l'URL
+        film_id = response.url.split("cfilm=")[-1].split(".html")[0]
+        # Construire l'URL du box office
+        box_office_url = f"https://www.allocine.fr/film/fichefilm-{film_id}/box-office/"
+        # Suivre le lien du box office pour accéder à la page du box office
+        yield scrapy.Request(box_office_url, callback=self.parse_box_office, meta={'title': title})
+
+    def parse_box_office(self, response):
+        # Extraire le box office fr de la première semaine
+        box_office = response.css("td[data-heading='Entrées']::text").get()
 
         allocine_movies_items = AllocineMovieItem()
-        allocine_movies_items['title'] = None
-        allocine_movies_items['date'] = None
-        allocine_movies_items['boxoffice'] = boxoffice
+        allocine_movies_items['title'] = response.meta['title']
+        allocine_movies_items['boxoffice'] = box_office.strip() if box_office else None
 
         yield allocine_movies_items
-
-    def parse_movie_details(self, response):
-        title = response.css('div.titlebar-title.titlebar-title-lg::text').get()
-        date = response.css('a.xXx.date.blue-link::text').get()
-        boxoffice = response.css('strong::text').get()
-
-        allocine_movies_items = AllocineMovieItem()
-        allocine_movies_items['title'] = title
-        allocine_movies_items['date'] = date
-        allocine_movies_items['boxoffice'] = boxoffice
     
-        yield allocine_movies_items
+
