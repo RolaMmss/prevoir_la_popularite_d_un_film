afb60ade7dbd2560fde5e6488a8aa2b35bf9c0a7
Noura Ous <noura.ousfia@gmail.com>
2023-07-21 15:01:30 +0200

    scaping titre origini et genre

--- scraping_data/scrap_imdb/scrap_imdb/spiders/spider_movies_250.py
+++ scraping_data/scrap_imdb/scrap_imdb/spiders/spider_movies_250.py
@@ -4,8 +4,6 @@ from ..items import ScrapImdbItem
 
 class ImdbSpiderSpider(scrapy.Spider):
 
-    
-
     name = "spider_movies_250"
     allowed_domains = ["allocine.fr"]
     user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0'
@@ -24,22 +22,15 @@ class ImdbSpiderSpider(scrapy.Spider):
 
 
 
-
-
     def parse_movie(self, response):
         items = ScrapImdbItem()
-        titre = response.xpath('(//div[@class="titlebar titlebar-page"]//div[@class="titlebar-title titlebar-title-lg"]//text())').get()
-        
+        titre = response.xpath('(//div[@class="titlebar titlebar-page"]//div[@class="titlebar-title titlebar-title-lg"]//text())').get() 
         date = response.css('span.date.blue-link::text').get()
-       # durée = response.xpath('//div[@class="meta-body-item meta-body-direction"] [1]').get()
-       
         réalisateur = response.css('span.light + span.blue-link::text').get()
         acteurs = response.css('div.meta-body-item.meta-body-actor span:not(.light)::text').getall()
-
         durée =  response.xpath('//div[@class="meta-body-item meta-body-info"]/span[@class="spacer"]/following-sibling::text()[1]').get()
-        genre = response.xpath('//div[@class="meta-body-item meta-body-info"]//a[@class="xXx"]').get()
-       # genre = response.xpath('//div[@class="meta-body-item meta-body-info"]/span[@class="spacer"]/following-sibling::text()[5]').getall()
-        titre_original = 1
+        genre = response.css('div.meta-body-item.meta-body-info span::text').getall()
+        titre_original = response.xpath('//span[@class="light" and contains(text(), "Titre original")]/following-sibling::text()').get()
         nationnalités = response.xpath('(//div[@class="item"]//span[@class="that"])[1]//text()').getall()
         type_film = response.xpath('(//div[@class="item"]//span[@class="that"])[6]//text()').get()
         langue_d_origine = response.xpath('(//div[@class="item"]//span[@class="that"])[8]//text()').get()
@@ -49,27 +40,6 @@ class ImdbSpiderSpider(scrapy.Spider):
         note_spectateurs = response.xpath('(//div[@class="rating-item-content"]//div[@class="stareval stareval-medium stareval-theme-default"]//span[@class="stareval-note"])[2]//text()').get()
         nombre_article = response.xpath('(//section[@class="section ovw"]//a[@class="end-section-link "])[2]//text()').get()
 
-
-
-
-
-
-       # item['release_date'] = release_date.strip() if release_date else None
-        # durée = response.xpath('(//ul[@class="ipc-inline-list ipc-inline-list--show-dividers sc-afe43def-4 kdXikI baseAlt"]/li)[3]//text()').get().strip('()')
-        
-        #date = response.xpath('(//div[@class="meta-body"]//div[@class="meta-body-item meta-body-info"]//a[@class="xXx date blue-link"])').get()
-        
-      #  date = response.xpath('(//div[@class="card entity-card entity-card-list cf entity-card-overview"]//div[@class="meta  "][1]//div[@class="meta-body"]//div[@class="meta-body-item meta-body-info"][1]//a[@class="xXx date blue-link"][1]  )').get()
-        # score = response.xpath('//a[@class="ipc-btn ipc-btn--single-padding ipc-btn--center-align-content ipc-btn--default-height ipc-btn--core-baseAlt ipc-btn--theme-baseAlt ipc-btn--on-textPrimary ipc-text-button sc-acdbf0f3-2 tBSnU"]//span[@class="ipc-btn__text"]//div[@class="sc-acdbf0f3-3 kpRihV"]//div[@class="sc-bde20123-2 gYgHoj"]//text()').get().strip('()')
-        # nbr_votants = response.xpath('(//a[@class="ipc-btn ipc-btn--single-padding ipc-btn--center-align-content ipc-btn--default-height ipc-btn--core-baseAlt ipc-btn--theme-baseAlt ipc-btn--on-textPrimary ipc-text-button sc-acdbf0f3-2 tBSnU"]//span[@class="ipc-btn__text"]//div[@class="sc-acdbf0f3-3 kpRihV"]//div[@class="sc-bde20123-3 bjjENQ"])//text()').get().strip('()')
-        # genre = response.xpath('(//div[@class="ipc-chip-list--baseAlt ipc-chip-list"]//div[@class="ipc-chip-list__scroller"])//text()').extract()
-        
-       # acteurs = response.css('(class="meta-body-item meta-body-direction")').extract()
-        # pays = response.xpath('(//ul[@class="ipc-metadata-list ipc-metadata-list--dividers-all ipc-metadata-list--base"]//li[@class="ipc-metadata-list__item"]//div[@class="ipc-metadata-list-item__content-container"]//ul[@class="ipc-inline-list ipc-inline-list--show-dividers ipc-inline-list--inline ipc-metadata-list-item__list-content base"]//li[@class="ipc-inline-list__item"]//a[@class="ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link"])[1]//text()').extract()
-        # langue_d_origine = response.xpath('(//ul[@class="ipc-metadata-list ipc-metadata-list--dividers-all ipc-metadata-list--base"]//li[@class="ipc-metadata-list__item"]//div[@class="ipc-metadata-list-item__content-container"]//ul[@class="ipc-inline-list ipc-inline-list--show-dividers ipc-inline-list--inline ipc-metadata-list-item__list-content base"]//li[@class="ipc-inline-list__item"]//a[@class="ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link"])[4]//text()').extract()
-        # budget = response.xpath('(//li[@class="ipc-metadata-list__item sc-6d4f3f8c-2 byhjlB"]//div[@class="ipc-metadata-list-item__content-container"]//li[@class="ipc-inline-list__item"]//span[@class="ipc-metadata-list-item__list-content-item"])[1]//text()').get()
-        # Sociétés_de_production = response.xpath('(//li[@class="ipc-metadata-list__item ipc-metadata-list-item--link"]//div[@class="ipc-metadata-list-item__content-container"]//ul[@class="ipc-inline-list ipc-inline-list--show-dividers ipc-inline-list--inline ipc-metadata-list-item__list-content base"]//li[@class="ipc-inline-list__item"]//a[@class="ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link"])[3]//text()').extract()       
-        
         def convertir_time(durée):
             if 'h' in durée and 'm' in durée:
                 heure, minute = durée.split("h ")
@@ -89,21 +59,24 @@ class ImdbSpiderSpider(scrapy.Spider):
      
         items['titre'] = titre
         items['date'] = date.strip() if date else None
-        items['titre_original'] = titre_original
+
+        if titre_original is not None:
+            items['titre_original'] = titre_original.replace('\n', '').strip()
+        else:
+            items['titre_original'] = None 
+
         items['durée'] = durée.strip()
-        # items['score'] = score
         items['type_film'] = type_film
-        # items['nbr_votants'] = nbr_votants
         items['réalisateur'] = réalisateur
         items['desciption'] = desciption.replace('\n', '').strip()
-        items['genre'] = genre
+        items['genre'] = [genre.strip() for genre in genre[4:]]
         items['acteurs'] = acteurs
         items['box_office_fr'] = box_office_fr
-       
         items['langue_d_origine'] = langue_d_origine.strip()
 
         cleaned_nationnalites = [nat.strip() for nat in nationnalités if nat.strip()]
         items['nationnalités'] = cleaned_nationnalites
+
         items['note_presse'] = note_presse 
         items['note_spectateurs'] = note_spectateurs
         items['nombre_article'] = nombre_article
