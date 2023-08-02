9032ec3fb010272c305db6d44b76379b2cecae64
Noura Ous <noura.ousfia@gmail.com>
2023-08-02 11:37:30 +0200

    base project django

--- project_cinema/myapp/forms.py
+++ project_cinema/myapp/forms.py
@@ -3,13 +3,19 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
 
-class SignupForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = get_user_model()         # la méthode utilitaire  get_user_model, qui vous permet d’obtenir le modèle  User sans l’importer directement
-        # fields = ('username', 'email', 'first_name', 'last_name')#, 'role')
-        fields = ('username', 'email')#, 'role')
+# class SignupForm(UserCreationForm):
+#     class Meta(UserCreationForm.Meta):
+#         model = get_user_model()         # la méthode utilitaire  get_user_model, qui vous permet d’obtenir le modèle  User sans l’importer directement
+#         # fields = ('username', 'email', 'first_name', 'last_name')#, 'role')
+#         fields = ('username', 'email')#, 'role')
 
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=63, label='Nom d’utilisateur')
-    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Mot de passe')
+# class LoginForm(forms.Form):
+#     username = forms.CharField(max_length=63, label='Nom d’utilisateur')
+#     password = forms.CharField(max_length=63, widget=forms.PasswordInput, label='Mot de passe')
     
+
+
+
+class UserCreateForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        fields = ("username","email", "password1", "password2")
