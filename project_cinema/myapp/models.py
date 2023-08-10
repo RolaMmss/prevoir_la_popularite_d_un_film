0c2a2277cb2f5933cd7198e1e319804d994e579f
Noura Ous <noura.ousfia@gmail.com>
2023-08-10 09:34:15 +0200

    commit model

--- project_cinema/myapp/models.py
+++ project_cinema/myapp/models.py
@@ -3,16 +3,15 @@ from django.utils import timezone
 
 
 
-
 class Film(models.Model):
     titre = models.CharField(max_length=500)
     distributeur = models.CharField(max_length=500)
-    date = models.DateField(default=timezone.now)
+   # date = models.DateField(default=timezone.now)
     type_film = models.CharField(max_length=100, default='Unknown')  # Add a default value here
 
 
     class Meta:
-        db_table = 'films'
+        db_table = 'dataset_model_ML'
 
 
 
@@ -24,8 +23,10 @@ class Acteurs_films(models.Model):
         db_table = 'acteurs_films'
 
 
+
 class Movies(models.Model):
     titre = models.CharField(max_length=500)
+
 
 
     class Meta:
